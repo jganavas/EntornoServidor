8bb3c6182f4e3195cd4d5bcd08f769febaa42183
Pepe <jganavas@gmail.com>
2025-10-21 19:49:50 +0200

    arreglo a su puta madre

--- Ejercicios/Tema2/Seminario/ej13.php
+++ Ejercicios/Tema2/Seminario/ej13.php
@@ -1,41 +1,39 @@
 <?php
 function generarHTML($cadena){
-    //$cadena = "div.coche cochaso primo#VWPolo";
-    $cadena = "div#VWPolo.coche cochaso primo";
-    $etiqueta = explode(".",$cadena)[0];
+    //Regex para conseguir la etiqueta padre (div, section, main...)
+    preg_match('/^([a-z]+)/', $cadena, $matches);
+    $etiqueta = $matches[1];
 
     $clases = "";
     $id = "";
 
-    //Longitud etiqueta padre (div, section, main...)
     $longitudEtiqueta = strlen($etiqueta);
 
+    //Caso para cuando primero encuentras las clases
     if($cadena[$longitudEtiqueta] == "."){
         $longitudEtiqueta++;
         while($cadena[$longitudEtiqueta] != "#"){
             $clases .= $cadena[$longitudEtiqueta++];
         }
         $longitudEtiqueta++;
-        while($cadena[$longitudEtiqueta] != $cadena[strlen($cadena)]){
+        while($longitudEtiqueta < strlen($cadena)){
             $id .= $cadena[$longitudEtiqueta++];
         }
-        //return "<" . $etiqueta . " class=" . $clases . " id=" . $id . "></" . $etiqueta . ">";
+        return '<' . $etiqueta . ' class="' . $clases . '" id="' . $id . '"></' . $etiqueta . '>';
     }
 
+    //Caso para cuando primero encuentras el ID
     if($cadena[$longitudEtiqueta] == "#"){
         $longitudEtiqueta++;
         while($cadena[$longitudEtiqueta] !== "."){
             $id .= $cadena[$longitudEtiqueta++];
         }
         $longitudEtiqueta++;
-        while($cadena[$longitudEtiqueta] !== $cadena[strlen($cadena)]){
+        while($longitudEtiqueta < strlen($cadena)){
             $clases .= $cadena[$longitudEtiqueta++];
         }
-        //return "<" . $etiqueta . " id=" . $id . " class=" . $clases . "></" . $etiqueta . ">";
+        return '<' . $etiqueta . ' id="' . $id . '" class="' . $clases . '"></' . $etiqueta . '>';
     }
-    echo $clases . "<br>";
-    echo $id;
 
 }
-
 echo generarHTML("div.coche cochaso primo#VWPolo");
